f594f79cf567162c6efffd764b7c3456d4975511
lucianomoral <lucianof.moral@gmail.com>
2021-10-31 14:11:05 -0300

    Heroku y configuraciones basicas iniciadas

--- distmoral/settings.py
+++ distmoral/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 django_heroku.settings(locals())
