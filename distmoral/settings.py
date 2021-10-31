e774354e0c7a3ddba4605f4a0bf2731f38de781c
lucianomoral <lucianof.moral@gmail.com>
2021-10-31 13:45:18 -0300

    Heroku y configuraciones basicas iniciadas

--- distmoral/settings.py
+++ distmoral/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 import django_heroku
 
 from pathlib import Path
@@ -121,6 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+
+STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 django_heroku.settings(locals())
